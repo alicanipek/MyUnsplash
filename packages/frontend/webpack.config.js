ecda6fb33b325ccd84aa93c651ad5fbf8db4ab7f
Ali Can İpek <alican.ipek@outlook.com>
2020-12-26 12:55:00 +0300

    frontend start

---
+++ packages/frontend/webpack.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+    entry: ['react-hot-loader/patch', './src/index.tsx'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.ts(x)?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        },
+                    },
+                    'postcss-loader',
+                ],
+            },
+            {
+                test: /\.svg$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.png$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            mimetype: 'image/png',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.tsx', '.ts'],
+    },
+    devServer: {
+        hot: true,
+        host: '0.0.0.0',
+        port: 1337,
+        disableHostCheck: true,
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+};
+
+module.exports = config;
